bd4f9ec84f0eaedce2ee26109ca854b2ff3719a6
suchananw <suchananw@users.github.com>
2018-11-17 16:29:49 +0700

    fix db user

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb://young&rich:a123456@ds211504.mlab.com:11504/movies-ticket",
+  mongoURI: "mongodb://youngrich:a123456@ds211504.mlab.com:11504/movies-ticket",
   secretOrKey: "secret"
 };
