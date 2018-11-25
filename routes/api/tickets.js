39a1cf1b4749a5d761a226661837680d3a62ed13
Supannee Mutitanon <supannee@dmit.co.th>
2018-11-25 08:06:23 +0700

    ticket api

--- routes/api/tickets.js
+++ routes/api/tickets.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const keys = require("../../config/keys");
 
 // Load Input Validation
-// const validateMovieInput = require("../../validation/movie");
+const validateTicketInput = require("../../validation/ticket");
 
 // Load User model
 const Ticket = require("../../models/Ticket");
@@ -17,35 +17,47 @@ router.get("/test", (req, res) => res.json({ msg: "ticket Works" }));
 // @desc    Add ticket
 // @access  Public
 router.post("/add", (req, res) => {
-    // const { errors, isValid } = validateMovieInput(req.body);
+    const { errors, isValid } = validateTicketInput(req.body);
   
     // // Check Validation
-    // if (!isValid) {
-    //   return res.status(400).json(errors);
-    // }
-    // Ticket.findOne({ name: req.body.name }).then(name => {
-    //   if (name) {
-    //     errors.name = "Movie already exists";
-    //     return res.status(400).json(errors);
-    //   } else {
-    //     const newMovie = new Movie({
-    //       name: req.body.name,
-    //       synopsis: req.body.synopsis,
-    //       startdate: req.body.startdate,
-    //       enddate: req.body.enddate,
-    //       length: req.body.length,
-    //       rate: req.body.rate,
-    //       genre: req.body.genre,
-    //       cinema: req.body.cinema,
-    //       poster: req.body.poster
-    //     });
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+        const newTicket = new Ticket({
+          movie: req.body.movie,
+          seat: req.body.seat,
+          cinema: req.body.cinema,
+          date: req.body.date,
+          showTime: req.body.showTime,
+          price: req.body.price,
+          bookingTime: req.body.bookingTime,
+          paid: req.body.paid,
+          user: req.body.user
+        });
   
-    //     newMovie
-    //       .save()
-    //       .then(movie => res.json(movie))
-    //       .catch(err => console.log(err));
-    //   }
-    // });
+        newTicket
+          .save()
+          .then(ticket => res.json(ticket))
+          .catch(err => console.log(err));
     });
 
+// @route   GET api/tickets/:ticketid/status
+// @desc    Return ticket status 
+// @access  Private
+router.post("/:ticketid/status", (req, res) => {
+    // const { errors, isValid } = validateMovieInput(req.body);
+  
+    // Check Validation
+    //if (!isValid) {
+    //return res.status(400).json(errors);
+   Ticket.findById(req.params.ticketid)
+      .then( ticket => {
+        if (!ticket) {
+          errors.noticket = "ticket not exists";
+          res.status(404).json(errors);
+        }
+        res.json(ticket.paid);
+      })
+      .catch(err => res.status(404).json({ ticket: "Ticket not exists" }));
+  });
 module.exports = router;
